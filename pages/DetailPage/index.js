555c6ef245d8f824e99b8adc990b03b680122b1b
Phat Phan Tan <46423166+phatptce130319@users.noreply.github.com>
2020-01-05 19:20:41 +0700

    Update index.js

--- pages/DetailPage/index.js
+++ pages/DetailPage/index.js
@@ -1,13 +1,27 @@
 import React, {useState} from 'react'
 import {View,Text} from 'react-native'
-const DetailPage = () => (
-    <View style={{flex:1, flexDirection: "column"}}>
+import config from '../../constant';
+import Carousel from 'react-native-snap-carousel'
+import DescribedCard from '../../components/DescribedCard';
+import { Dimensions } from "react-native";
+
+const DetailPage = () => {
+    const [items, setItems] = useState(config.sampleItems)
+    const RenderItem = ({item, index}) => (
+    <DescribedCard style={{flex: 1, flexDirection: 'column'}} {...item}/>
+)
+    return (
+        <View style={{flex:1, flexDirection: "column"}}>
         <View style={{flex: 1}}>
-            <Text>ABC</Text>
+        <Carousel style={{flex: 1}} data={items} sliderWidth={Dimensions.get('window').width}
+            itemWidth={250} layout={'default'} renderItem={RenderItem} loop={true}/>
         </View>
         <View style={{flex: 2}}>
         <Text>XYZ</Text>
         </View>
     </View>
-)
+    )
+}
+    
+
 export default DetailPage
