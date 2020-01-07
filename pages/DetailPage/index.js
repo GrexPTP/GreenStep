bb332e3b0aa6c082709d4463e622966b06a3b310
Phat Phan Tan <46423166+phatptce130319@users.noreply.github.com>
2020-01-07 21:24:16 +0700

    Add google map key and load the map successful

--- pages/DetailPage/index.js
+++ pages/DetailPage/index.js
@@ -1,23 +1,47 @@
 import React, {useState} from 'react'
-import {View,Text} from 'react-native'
+import {View,  ScrollView} from 'react-native'
 import config from '../../constant';
 import Carousel from 'react-native-snap-carousel'
 import DescribedCard from '../../components/DescribedCard';
 import { Dimensions } from "react-native";
-
+import Map from '../../components/Map'
+import {Avatar, Card, Title, Paragraph, Text, Button, ProgressBar, Colors } from 'react-native-paper'
 const DetailPage = () => {
     const [items, setItems] = useState(config.sampleItems)
     const RenderItem = ({item, index}) => (
     <DescribedCard style={{flex: 1, flexDirection: 'column'}} {...item}/>
 )
+
     return (
         <View style={{flex:1, flexDirection: "column"}}>
         <View style={{flex: 1}}>
         <Carousel style={{flex: 1}} data={items} sliderWidth={Dimensions.get('window').width}
             itemWidth={250} layout={'default'} renderItem={RenderItem} loop={true}/>
         </View>
-        <View style={{flex: 2}}>
-        <Text>XYZ</Text>
+        <View style={{flex: 2, marginTop: 5}}>
+        <ScrollView>
+        <Card style={{margin: 10}} >
+                          <Card.Title title={'General Info'} titleStyle={{color: 'red'}} />
+                          <Card.Content>
+                            <Paragraph style={{fontSize:18}}>{`Program: Name of program`}</Paragraph>
+                            <Paragraph style={{fontSize:18}}>{`Date: 22/08/2020`}</Paragraph>
+                          </Card.Content>
+                        </Card>
+                        <Card style={{height: 200,margin: 10}} >
+                          <Card.Title title={'Detail Info'} titleStyle={{color: 'red'}} />
+                          <Card.Content>
+                                <Paragraph>
+                                    This is details of the events
+                                </Paragraph>
+                          </Card.Content>
+                        </Card>
+                        <Button icon="door" mode="text" onPress={() => console.log('Pressed')}>
+    Participate
+  </Button>
+  <ProgressBar progress={0.5} color={Colors.red800}/>
+  <Map/>
+        </ScrollView>
+        
         </View>
     </View>
     )
