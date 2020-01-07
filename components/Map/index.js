f748e0b839f8527ae926183ac9eb474617d454c4
Phat Phan Tan <46423166+phatptce130319@users.noreply.github.com>
2020-01-07 21:47:11 +0700

    Add get current position

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import MapView , {Marker}from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Platform } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions'
+import Constants from 'expo-constants';
 
 export default class Map extends React.Component {
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.mapStyle} initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
+        {this.state.location ? <MapView style={styles.mapStyle} initialRegion={{
+      latitude: this.state.location.coords.latitude,
+      longitude: this.state.location.coords.longitude,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     }}>
       <Marker coordinate={{
-  latitude: 37.78825,
-  longitude: -122.4324,
+  latitude: this.state.location.coords.latitude,
+  longitude: this.state.location.coords.longitude,
 }}
       title={'TEST'}
       description={'TEST DES'}></Marker>
-    </MapView>
+    </MapView> : null}
+        
+    
       </View>
     );
   }
